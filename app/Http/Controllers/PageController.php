f40b672d76f4e8424713f801d630f6b137c18ee2
Rizqy Faishal <rizqyfaishal@hotmail.com>
2016-07-27 10:22:56 -0700

    adding some new routes page

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\PageDescription;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PageController extends Controller
+{
+    public function __construct(PageDescription $page){
+        $this->page = $page;
+    }
+
+    public function index(){
+        $this->page->setTitle('Home');
+        return view('index')->with([
+            'page' => $this->page
+        ]);
+    }
+
+    public function bookingStatus(){
+        $this->page->setTitle('Booking Status');
+        return view('booking-status')->with([
+            'page' => $this->page
+        ]);
+    }
+
+    public function features(){
+        $this->page->setTitle('Features');
+        return view('features')->with([
+            'page' => $this->page
+        ]);
+    }
+
+    public function login(){
+        $this->page->setTitle('Login');
+        return view('login')->with([
+            'page' => $this->page
+        ]);
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('UserArea.bayar');
-});
+Route::get('/', 'PageController@index');
 
 Route::controllers([
     'auth' => 'Auth\AuthController',
