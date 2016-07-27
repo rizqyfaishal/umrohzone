4fcbb1356791f6ad9edba0cb58b5621d02670369
Rizqy Faishal <rizqyfaishal@hotmail.com>
2016-07-27 10:39:37 -0700

    adding dependency library [:laravel collective,:pdf-generator]

--- app/Helper/PageDescription.php
+++ app/Helper/PageDescription.php
@@ -1,36 +1,84 @@
 <?php
+
+namespace App\Helper;
 /**
  * Created by PhpStorm.
  * User: Rizqy
- * Date: 4/17/2016
- * Time: 11:53 AM
+ * Date: 6/25/2016
+ * Time: 12:12 AM
  */
-
-namespace App\Helper;
-
-
 class PageDescription
 {
-
-    public function __construct(NavigationStatus $status){
-        $this->status = $status;
+    public function __construct(){
+        $this->title = null;
+        $this->author = 'Anak anak Fasilkom';
+        $this->description = 'Bla-bla';
+        $this->keyword = 'sansaksnaksa';
     }
 
-    private $page = [
-        'title'         => '',
-        'description'   => 'Klikacara.com merupakan sebuah website',
-        'author'        => 'Fukicorp.id',
-        'keyword'       => 'klikacara.com, event, mediapartners',
-//        'status'        => null
-    ];
-
-    public function setPage($title){
-        $this->page['title']    = $title;
-//        $this->page['status'] = $this->status->setStateActive($active_key);
-        return $this->page;
+    /**
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
     }
 
-    public function getPageData(){
-        return $this->page;
+    /**
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
     }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * @param string $keyword
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = $keyword;
+    }
+
+    /**
+     * @return null
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param null $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+
 }
